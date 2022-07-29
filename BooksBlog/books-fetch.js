2d6acf9502033f4ad30a18390af8efca83fbddca
Tsvetomir Krumov <tsvetomir.krumov@Tsvetomirs-Mac-mini.local>
2022-07-29 17:54:58 +0300

    My Library + API requests added

--- BooksBlog/books-fetch.js
+++ BooksBlog/books-fetch.js
@@ -1,34 +1,40 @@
 function genBook(book) {
-    const resultsElem = document.getElementById("results");
-    const art = document.createElement("article");
-    console.log(art);
-    art.innerHTML =
-    `
+  const art = document.createElement("article");
+  console.log(art);
+  art.innerHTML = `
     <h1> Title: ${book.volumeInfo.title}</h1>
     <h2> Author: ${book.volumeInfo.authors}</h2>
     <section>
     <img src="${book.volumeInfo.imageLinks.thumbnail}"></img>
-    <p>${book.volumeInfo.description.slice(0,500)}
+    <p>${book.volumeInfo.description.slice(0,300)}
     </p>
     </section>
-    `
-return art;
-  }
+    `;
+  return art;
+}
 
-  async function init() {
-    try {
+async function init(searchText) {
+  try {
     const resultsElem = document.getElementById("results");
-    const booksResp = await fetch("books-data.json")
-    const books = await booksResp.json()
-    books.items.forEach(element => {
-        resultsElem.appendChild(genBook(element));
+    const booksResp = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+        searchText
+      )}&maxResults=15`
+    );
+    const books = await booksResp.json();
+    resultsElem.innerHTML='';
+    books.items.forEach((element) => {
+      resultsElem.appendChild(genBook(element));
     });
     console.log(books);
-  } catch(err) {
-    console.log('Error', err);
-  } finally{
-    console.log('Demo finished')
+  } catch (err) {
+    console.log("Error", err);
+  } finally {
+    console.log("Demo finished");
   }
-  }
-
-  init();
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  const searchValue = document.getElementById("search").value;
+  init(searchValue);
+}
--- task-03-js/github-async-demo.js
+++ task-03-js/github-async-demo.js
@@ -1,30 +1,54 @@
+import { User } from "./user.js";
+
 async function init() {
   try {
-  const resultsElem = document.getElementById("results");
-  const usersResp = await fetch("users.json")
-  const users = await usersResp.json()
-  console.log(users);
-      gitUsers = Promise.all(
-        users.map(async user => {
-           const gitUserResp = await fetch(`https://api.github.com/users/${user.username}`);
-          return gitUserResp.json();
-        })
-      );
-      console.log(gitUsers);
-      const images = gitUsers.map((gitUser) => {
-        const img = new Image();
-        img.src = gitUser.avatar_url;
-        resultsElem.insertAdjacentElement("beforeend", img);
-        return img;
-      });
-      await new Promise((resolve, reject) => {
-        setTimeout(resolve, 5000, images);
-      });
-      images.forEach((img) => resultsElem.removeChild(img));
-  } catch(err) {
-    console.log('Error:', err);
+    const resultsElem = document.getElementById("results");
+    const usersResp = await fetch("users.json");
+    const users = await usersResp.json();
+    console.log(users);
+    const gitUsers =  await Promise.all(
+      users.map(async (user) => {
+        const gitUserResp = await fetch(
+          `https://api.github.com/users/${user.username}`
+        );
+        const gu = await gitUserResp.json();
+        return new User(
+          gu.login,
+          gu.name,
+          gu.avatar_url,
+          gu.public_repos,
+          gu.public_gists,
+          gu.followers
+        );
+      })
+    );
+    console.log(gitUsers);
+    const elems = gitUsers.map(u => {
+      const userDiv = document.createElement("div");
+      userDiv.innerHTML = `
+        <figure>
+        <img src =${u.pictureUrl}>
+        <figcaption>${u.name} - ${u.username}
+        <br>
+        Public Repos:${u.public_repos}
+        <br>
+        Public Gists:${u.public_gists}
+        <br>
+        Followers: ${u.followers}
+        </figcaption>
+        </figure>
+        `;
+      resultsElem.insertAdjacentElement("beforeend", userDiv);
+      return userDiv;
+    });
+    // await new Promise((resolve, reject) => {
+    //   setTimeout(resolve, 5000, images);
+    // });
+    // elems.forEach((elem) => resultsElem.removeChild(elem));
+  } catch (err) {
+    console.log("Error:", err);
   } finally {
     console.log("Demo finished");
   }
 }
-init()
+init();
